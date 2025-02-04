8e11ad943d9a22723d0116464fdaafc251f3c854
Asim Sikka <asim@amroodlabs.com>
2025-02-04 14:59:26 +0500

    Zeitwerk fix for rails 7

--- app/overrides/add_comment_to_admin_users_tabs.rb
+++ app/overrides/add_comment_to_admin_users_tabs.rb
@@ -1,5 +1,7 @@
-Deface::Override.new(:virtual_path => "spree/admin/users/_sidebar",
-                    :name => "add_comment_to_admin_users_tabs",
-                    :insert_bottom => "[data-hook='admin_user_tab_options'], #admin_user_tab_options[data-hook]",
-                    :partial => "spree/admin/users/tab",
-                    :disabled => false)
+class AddCommentToAdminUsersTabs
+  Deface::Override.new(:virtual_path => "spree/admin/users/_sidebar",
+                      :name => "add_comment_to_admin_users_tabs",
+                      :insert_bottom => "[data-hook='admin_user_tab_options'], #admin_user_tab_options[data-hook]",
+                      :partial => "spree/admin/users/tab",
+                      :disabled => false)
+end
